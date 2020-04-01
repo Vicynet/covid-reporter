96cecc193fb522cb58cafcdc1b6e99a4be41df1b
Victor Ihedioha <ihedioha.victor@gmail.com>
2020-04-01 22:09:20 +0100

    Worked on messy API call

--- covidjs/covidjs.js
+++ covidjs/covidjs.js
@@ -1,6 +1,31 @@
 var covid_date = new Date();
 document.getElementById("date").innerHTML = covid_date.toDateString();
 
+var request = new XMLHttpRequest()
+
+var death = parseInt(document.getElementById("death").innerHTML)
+var recovered = parseInt(document.getElementById("recover").innerHTML)
+var active = death + recovered
+document.getElementById("closed").innerHTML = active
+
+request.open('GET', 'https://api.covid19api.com/live/country/nigeria/status/confirmed', true)
+request.onload = function() {
+  // Begin accessing JSON data here
+  var data = JSON.parse(this.response)
+
+  if (request.status >= 200 && request.status < 400) {
+    data.forEach(Country => {
+      document.getElementById("cases").innerHTML = Country.Cases
+      // document.getElementById("active").innerHTML = Country.Cases - active
+    })
+  } else {
+    console.log('error')
+  }
+}
+
+request.send()
+
+
 //Create the XHR Object
 let xhr = new XMLHttpRequest;
 
@@ -14,7 +39,8 @@ xhr.onload = function()
         // check if the status is 200(means everything is okay)
         if (this.status === 200) 
             {
-                  document.getElementById("cases").innerHTML = data.total
+                  // document.getElementById("cases").innerHTML = data.total
+                  // var all = document.getElementById("cases").innerHTML
                   document.getElementById("death").innerHTML = data.death
                   document.getElementById("recover").innerHTML = data.discharged
 
@@ -63,8 +89,3 @@ req.onload = function()
 req.send();
 //Common Types of HTTP Statuses
 // 200: OK
-
-        
-
-
-
