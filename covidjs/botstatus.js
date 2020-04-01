844d697b06735cc1ffddb7f78f3223bb95edd1e2
Victor Ihedioha <ihedioha.victor@gmail.com>
2020-04-01 14:36:54 +0100

    Files Merge Into One, RedundantFiles Removed

--- covidjs/botstatus.js
+++ covidjs/botstatus.js
@@ -1,19 +1,19 @@
 
-	var questionCounter = 20;
+var questionCounter = 20;
 
-	function submitQuiz(){
-		var elems = document.getElementsByClassName('stat');
-		var myLength = elems.length,
-		total = 0;
-	
-		for (var i = 0; i < myLength; ++i) {
-		  total += parseInt(elems[i].options[elems[i].selectedIndex].value * 1,10);
-		}
-		var showScore = "Your Score: " + total +"/" + questionCounter;
+function submitQuiz(){
+	var elems = document.getElementsByClassName('stat');
+	var myLength = elems.length,
+	total = 0;
 
-		document.getElementById('userInput').value = showScore;
-		
+	for (var i = 0; i < myLength; ++i) {
+		total += parseInt(elems[i].options[elems[i].selectedIndex].value * 1,10);
 	}
+	var showScore = "Your Score: " + total +"/" + questionCounter;
+
+	document.getElementById('userInput').value = showScore;
+	
+}
 
 $(document).ready(function() {
 
--- covidjs/covidjs.js
+++ covidjs/covidjs.js
@@ -1,60 +1,68 @@
 var covid_date = new Date();
 document.getElementById("date").innerHTML = covid_date.toDateString();
 
-var request = new XMLHttpRequest()
+//Create the XHR Object
+let xhr = new XMLHttpRequest;
 
-var death = parseInt(document.getElementById("death").innerHTML)
-var recovered = parseInt(document.getElementById("recover").innerHTML)
-var active = death + recovered
-document.getElementById("closed").innerHTML = active
+// Call the open function, GET-type of request, url, true-asynchronous
+xhr.open('GET', 'https://covid19-update-api.herokuapp.com/api/v1/ncdc/cases', true)
+// call the onload 
+xhr.onload = function() 
+    {
+      let data = JSON.parse(this.responseText).cases;
 
-request.open('GET', 'https://api.covid19api.com/live/country/nigeria/status/confirmed', true)
-request.onload = function() {
-  // Begin accessing JSON data here
-  var data = JSON.parse(this.response)
+        // check if the status is 200(means everything is okay)
+        if (this.status === 200) 
+            {
+                  document.getElementById("cases").innerHTML = data.total
+                  document.getElementById("death").innerHTML = data.death
+                  document.getElementById("recover").innerHTML = data.discharged
 
-  if (request.status >= 200 && request.status < 400) {
-    data.forEach(Country => {
-      document.getElementById("cases").innerHTML = Country.Cases
-      document.getElementById("active").innerHTML = Country.Cases - active
-    })
-  } else {
-    console.log('error')
-  }
-}
-
-request.send()
-
+                  var death = parseInt(document.getElementById("death").innerHTML)
+                  var recovered = parseInt(document.getElementById("recover").innerHTML)
+                  var all = parseInt(document.getElementById("cases").innerHTML)
+                  var active = all - (death + recovered)
+                  document.getElementById("active").innerHTML = active
+                  document.getElementById('closed').innerHTML = death + recovered
+               
+              } else {
+                console.log('error')
+    }
+            }
+// call send
+xhr.send();
 
 //Create the XHR Object
-// let xhr = new XMLHttpRequest;
+let req = new XMLHttpRequest;
 
 //Call the open function, GET-type of request, url, true-asynchronous
-// xhr.open('GET', 'https://covid19-update-api.herokuapp.com/api/v1/ncdc/cases', true)
+req.open('GET', 'https://covid19-update-api.herokuapp.com/api/v1/ncdc/cases/states', true)
 //call the onload 
-// xhr.onload = function() 
-    // {
-      // let data = JSON.parse(this.responseText).cases;
-
+req.onload = function() 
+    {
         //check if the status is 200(means everything is okay)
-        // if (this.status === 200) 
-    //         {
-    //               document.getElementById("cases").innerHTML = data.total
-    //               document.getElementById("death").innerHTML = data.death
-    //               document.getElementById("recover").innerHTML = data.discharged
+        if (this.status === 200) 
+            {
+                  let data = JSON.parse(this.responseText).states;
+                  var x = ""
+                  var y = ""
 
-    //               var death = parseInt(document.getElementById("death").innerHTML)
-    //               var recovered = parseInt(document.getElementById("recover").innerHTML)
-    //               var all = parseInt(document.getElementById("cases").innerHTML)
-    //               var active = all - (death + recovered)
-    //               document.getElementById("active").innerHTML = active
-               
-    //           } else {
-    //             console.log('error')
-    // }
-    //         }
+                for (i in data) {
+                    if (data != 0) {
+                        x += data[i].name + "<br>";
+                        y += data[i].cases + "<br>";
+                    }
+                 
+                  }
+                
+                document.getElementById("states").innerHTML = x;
+                document.getElementById("state_cases").innerHTML = y;
+    }
+            }
 //call send
-// xhr.send();
+req.send();
+//Common Types of HTTP Statuses
+// 200: OK
 
         
 
